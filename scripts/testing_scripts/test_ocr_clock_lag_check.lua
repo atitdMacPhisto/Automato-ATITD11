a8fd99cb56b55033f24669d3252b0a9b2a77b229
atitdRhaom <atitdRhaom>
2023-04-10 21:23:22 +0100

    OCR Bits & Stuff

--- scripts/testing_scripts/test_ocr_clock_lag_check.lua
+++ scripts/testing_scripts/test_ocr_clock_lag_check.lua
@@ -1,9 +1,12 @@
 dofile("common.inc");
-	
-function doit()
 
-askForWindow("Parses the game clock and extracts the info.\nInfo updates in realtime while running!\nPress Shift over ATITD to continue.");
+askText = "Parses the game clock and extracts the info.\n\n"
+.. "Info updates in realtime while running!\n\n"
+.. "Press Shift over ATITD to continue."
 
+----------------------------------------
+--          Global Variables          --
+----------------------------------------
 elapsedTime = 0;
 startTime = 0;
 lastTime = 0;
@@ -16,64 +19,64 @@ duckTime = 0;
 duckTimeSeconds = 0;
 duckTimeAvg = 0;
 minsElapsedGUI = "Awaiting Change";
-message = "";
-message2 = "";
 scriptStartTime = lsGetTimer();
+----------------------------------------
 
-	findClockInfo();
-	while 1 do
-        local pos = getMousePos();
-        lastTime = Time;
-        findClockInfo();
+function doit()
+  askForWindow(askText);
+  findClockInfo();
+  while 1 do
+  local pos = getMousePos();
+  lastTime = Time;
+  findClockInfo();
 
-        if minsElapsed == -1 then
-          scriptStartTime = lsGetTimer()
-        end
+  if minsElapsed == -1 then
+    scriptStartTime = lsGetTimer()
+  end
 
-        if lastTime ~= Time then
-          minsElapsed = minsElapsed + 1;
-	     if minsElapsed == 0 then
-	       minsElapsedGUI = "Starting Timer";
-	       scriptStartTime = lsGetTimer()
-	     else
-	       minsElapsedGUI = minsElapsed;
-	     end
+  if lastTime ~= Time then
+    minsElapsed = minsElapsed + 1;
+    if minsElapsed == 0 then
+      minsElapsedGUI = "Starting Timer";
+      scriptStartTime = lsGetTimer()
+    else
+      minsElapsedGUI = minsElapsed;
+    end
 
-	     if minsElapsed == 0 then
-	       startTime = lsGetTimer();
-	     elseif minsElapsed >= 1 then
-	       elapsedTime = lsGetTimer() - startTime
-	       elapsedTimeSeconds = math.floor(elapsedTime/100)/10;
-	       elapsedTimeTotal = elapsedTimeTotal + elapsedTime;
-	       elapsedTimeAvg = elapsedTimeTotal / minsElapsed;
-	       duckTime = elapsedTime * 3;
-	       duckTimeSeconds = math.floor(duckTime/100)/10;
-	       duckTimeAvg = (elapsedTimeTotal * 3) / minsElapsed;
-	       elapsedTimeAvg = math.floor(elapsedTimeAvg/100)/10;
-	       duckTimeAvg = math.floor(duckTimeAvg/100)/10;
-	       startTime = lsGetTimer();
-	     end
-        end
+    if minsElapsed == 0 then
+      startTime = lsGetTimer();
+    elseif minsElapsed >= 1 then
+      elapsedTime = lsGetTimer() - startTime
+      elapsedTimeSeconds = math.floor(elapsedTime/100)/10;
+      elapsedTimeTotal = elapsedTimeTotal + elapsedTime;
+      elapsedTimeAvg = elapsedTimeTotal / minsElapsed;
+      duckTime = elapsedTime * 3;
+      duckTimeSeconds = math.floor(duckTime/100)/10;
+      duckTimeAvg = (elapsedTimeTotal * 3) / minsElapsed;
+      elapsedTimeAvg = math.floor(elapsedTimeAvg/100)/10;
+      duckTimeAvg = math.floor(duckTimeAvg/100)/10;
+      startTime = lsGetTimer();
+    end
+  end
 
+  while not coordinates or not fetchTime do
+    findClockInfo()
+    sleepWithStatus(999, "We have a problem finding Clock!", nil, 0.7, "Error Reading Clock")
+  end
 
-	while not coordinates or not fetchTime do
-	  findClockInfo()
-	  sleepWithStatus(999, "We have a problem finding Clock!", nil, 0.7, "Error Reading Clock")
-	end
-
-        sleepWithStatus(100, "Mouse Pos: " .. pos[0] .. ", " .. pos[1] .. "\n\nYear: " .. year .. "\nDate: " .. Date .. "\nTime: " .. Time .. "\nRegion: " .. regionName .. "\nCoords: " 
-	  .. Coordinates .. "\nFaction: " .. faction .. "\n\nElapsed Egypt Mins: " .. minsElapsedGUI .. "\nElapsed Real Time: " .. getElapsedTime(scriptStartTime) .. "\nLast Secs per Egypt Minute: " .. elapsedTimeSeconds .. "\nAvg Secs per Egypt Minute: " .. elapsedTimeAvg .. "\nLast Secs per Game Minute: " 
-	  .. duckTimeSeconds .. "\nAvg Secs per Game Minute: " .. duckTimeAvg .. "\nAvg Teppy/Duck Multiplier: " .. round(duckTimeAvg/60,4), nil, 0.67, "Reading Clock");
-
-	end
+  sleepWithStatus(100, "Mouse Pos: " .. pos[0] .. ", " .. pos[1] .. "\n\nYear: " .. year .. "\nDate: " .. Date ..
+  "\nTime: " .. Time .. "\nRegion: " .. regionName .. "\nCoords: " .. Coordinates ..
+  "\n\nElapsed Egypt Mins: " .. minsElapsedGUI .. "\nElapsed Real Time: " .. getElapsedTime(scriptStartTime) ..
+  "\nLast Secs per Egypt Minute: " .. elapsedTimeSeconds .. "\nAvg Secs per Egypt Minute: " .. elapsedTimeAvg ..
+  "\nLast Secs per Game Minute: " .. duckTimeSeconds .. "\nAvg Secs per Game Minute: " .. duckTimeAvg ..
+  "\nAvg Teppy/Duck Multiplier: " .. round(duckTimeAvg/60,4), nil, 0.67, "Reading Clock");
+  end
 end
-
 
 function round(num, numDecimalPlaces)
   local mult = 10^(numDecimalPlaces or 0)
   return math.floor(num * mult + 0.5) / mult
 end
-
 
 function findClockInfo()
   srReadScreen();
@@ -88,7 +91,8 @@ function findClockInfo()
   fetchTime = getTime(1);
   if fetchTime ~= nil then
     year = string.match(fetchTime, "Year (%d+)")
-    theDateTime = string.sub(fetchTime,string.find(fetchTime,",") + 0); -- I know it's weird to have +0, but don't remove it or will error, shrug
+    -- I know it's weird to have +0, but don't remove it or will error, shrug
+    theDateTime = string.sub(fetchTime,string.find(fetchTime,",") + 0);
     stripYear = string.sub(theDateTime,string.find(theDateTime,",") + 2);
     Time = string.sub(stripYear,string.find(stripYear,",") + 2);
     stripYear = "," .. stripYear
@@ -96,7 +100,6 @@ function findClockInfo()
     stripYear = string.sub(theDateTime,string.find(theDateTime,",") + 2);
   end
 end
-
 
 --Custom sleepWithStatus to add extra boxes, buttons, etc
 local waitChars = {"-", "\\", "|", "/"};
@@ -127,14 +130,6 @@ function sleepWithStatus(delay_time, message, color, scale, waitMessage)
     end
     lsPrintWrapped(10, 50, 0, lsScreenX - 20, scale, scale, 0xd0d0d0ff,
                    newWaitMessage .. waitChars[frame]);
-
-		if faction == "Hyksos" then
-		  lsButtonImg(170, 110, 1, 0.20, 0xFFFFFFff, "faction/hyksos.png");
-		elseif faction == "Kush" then
-		  lsButtonImg(170, 110, 1, 0.17, 0xFFFFFFff, "faction/kush.png");
-		elseif faction == "Meshwesh"  then
-		  lsButtonImg(170, 110, 1, 0.20, 0xFFFFFFff, "faction/meshwesh.png");
-		end
 
     statusScreen(message, color, nil, scale);
     lsSleep(tick_delay);
