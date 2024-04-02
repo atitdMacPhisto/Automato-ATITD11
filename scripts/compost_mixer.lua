6f8414f8dedd3573f973710d6040b5f9d99434bd
Ashen <ashenatitd@gmail.com>
2024-04-02 14:21:08 -0700

    Add reading and display of values from the compost mixer including a value for soil type that can be used with the calculator. Add a stop button and stop erroring out if the mixer window isn't found.

--- scripts/compost_mixer.lua
+++ scripts/compost_mixer.lua
@@ -1,4 +1,5 @@
 dofile("common.inc");
+dofile("settings.inc");
 
 askText = [[Compost Mixer
 
@@ -27,13 +28,87 @@ mixer_ingredients = {
 	"Salts of Al"	
 };
 
-function getMixerWin()
-	local mixerWin = findAllText("Compost Mixer");
-	if #mixerWin < 1 then
-		error("Mixer window not found");
+SOILWIDTH = 366;
+SOILCOLOR = 0x32D184;
+
+mix_metal = "";
+mix_ph = "";
+mix_salinity = "";
+mix_water = "";
+mix_nitrogen = "";
+mix_phosphorus = "";
+mix_metal = "";
+mix_potassium = "";
+mix_soot = "";
+mix_eco = "";
+
+function getSoilValue(win)
+	srReadScreen();
+	local pos = srFindImage("soiltype.png");
+
+	while not pos do
+		checkBreak();
+		srReadScreen();
+		sleepWithStatus(1000, "Soil Type not found/obscured");
+		pos = srFindImage("soiltype.png");
 	end
-	if #mixerWin > 1 then
-		error("Too many mixer windows");
+	
+	srSetMousePos(pos[0], pos[1]);
+
+	pos[0] = pos[0] + 75;
+	pos[1] = pos[1] - 3;
+
+	local base = pos[0];
+
+	local ofs = makePoint(0, 0);
+	local val = 0;
+	local left = nil;
+	local right = nil;
+	
+	srReadScreen();
+	for i=1,SOILWIDTH+10 do
+		if pixelMatchFromBuffer(pos, ofs, SOILCOLOR, 0) then
+			if left == nil then
+				left = ofs[0];
+			end
+			right = ofs[0];
+		end
+		ofs[0] = ofs[0] + 1;
+	end
+
+	if (left ~= nil) and (right ~= nil) then
+		val = right - left;
+
+		val = math.ceil(100.0 * val / (SOILWIDTH / 2.0));
+
+		if (val >= 98) then
+			val = 100;
+		end
+
+		if (left < SOILWIDTH/2) then
+			val = val * -1;
+		end
+	end
+
+	return val;
+end
+
+function getMixerWin()
+	srReadScreen();
+	local mixerWin = findAllText("Compost Mixer");
+
+	while #mixerWin ~= 1 do
+		checkBreak();
+
+		srReadScreen();
+		mixerWin = findAllText("Compost Mixer");
+		
+		if #mixerWin < 1 then
+			sleepWithStatus(1000, "Mixer window not found");
+		end
+		if #mixerWin > 1 then
+			sleepWithStatus(1000, "Too many mixer windows");
+		end
 	end
 
 	return getWindowBorders(mixerWin[1][0], mixerWin[1][1]);
@@ -61,9 +136,13 @@ function doAdd(what, count)
 			lsPlaySound("fail.wav");
 			sleepWithStatus(8000, "Insufficient ingredients on hand!");
 			clickAllImages("ok.png");
-			return;
+			return i - 1;
 		end
 
+		if lsButtonText(10, lsScreenY - 30, 0, 100, 0xFF0000ff, "Stop") then
+			return i;
+		end
+		
 		if lsButtonText(lsScreenX - 110, lsScreenY - 30, 0, 100, 0xFFFFFFff, "End script") then
 			error "Clicked End Script button";
 		end
@@ -71,17 +150,25 @@ function doAdd(what, count)
 		lsDoFrame();
 		lsSleep(50);
 	end
+
+	return count;
 end
 
 function doit()
 	askForWindow(askText);
-	getMixerWin();
 
 	local count = 1;
 	local what = 1;
+	local soil = 0;
 
+	count = readSetting("count", count);
+	what = readSetting("what", what);
+	
 	while true do
 		checkBreak();
+
+		win = getMixerWin();
+		soil = getSoilValue(win);
 		
 		y = 10;
 		lsPrint(10, y, 10, 0.7, 0.7, 0xFFFFFFff, "Qty");
@@ -102,8 +189,15 @@ function doit()
 			count = tonumber(count)
 		end
 
+		y = y + 30;
+		lsPrint(10, y, 10, 0.7, 0.7, 0xFFFFFFff, "SOIL: " .. soil);
+
 		if lsButtonText(10, lsScreenY - 30, 0, 100, 0x00FF00ff, "ADD IT") and count > 0 then
-			doAdd(what, count);
+			writeSetting("count", count);
+			writeSetting("what", what);
+			count = count - doAdd(what, count);
+			writeSetting("count", count);
+			lsEditBoxSetText('qty', count);
 		end
 
 		if lsButtonText(lsScreenX - 110, lsScreenY - 30, 0, 100, 0xFFFFFFff, "End script") then
